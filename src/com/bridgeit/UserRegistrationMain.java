abe6afd551d86720db109982369ccb988e87de5d
musai7 <musaishankar307@gmail.com>
2021-11-23 20:54:04 +0530

    added : last name pattern

--- src/com/bridgeit/UserRegistrationMain.java
+++ src/com/bridgeit/UserRegistrationMain.java
@@ -1,33 +1,12 @@
 package com.bridgeit;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class UserRegistrationMain {
-
-	List<UserRegistration> list;
-
-	public UserRegistrationMain() {
-		list = new ArrayList<>();
-	}
 
 	public static void main(String[] args) {
 
-		UserRegistration registration = new UserRegistration();
-
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("enter First Name");
-
-		while (true) {
-			String fName = scanner.next();
-			if (PatternCheck.firstName(fName)) {
-				registration.setFirstName(fName);
-				break;
-			} else {
-				System.out.println("enter valid First Name as --> Xyz...");
-			}
-		}
-		System.out.println(registration);
+		PatternCheck check = new PatternCheck();
+		check.firstNamePattern();
+		check.lastNamePattern();
+		check.printResult();
 	}
 }
