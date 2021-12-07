e5b3995950e539d76ef61576e39ba4ccb98b61c1
musai7 <musaishankar307@gmail.com>
2021-12-07 12:05:24 +0530

    refactored : the code to custome exceptions of invalid user details

--- src/com/bridgeit/InvalideUserDetailsException.java
+++ src/com/bridgeit/InvalideUserDetailsException.java
@@ -2,6 +2,13 @@ package com.bridgeit;
 
 public class InvalideUserDetailsException extends Exception {
 
+	enum ExeptionType{
+		FIRST_NAME,
+		LAST_NAME,
+		PHONE_NUMBER,
+		PASSWORD,
+		EMAIL_ID
+	}
 	public InvalideUserDetailsException(String data) {
 		
 		super(data);
--- src/com/bridgeit/UserRegestrationPatterns.java
+++ src/com/bridgeit/UserRegestrationPatterns.java
@@ -1,8 +1,6 @@
 
 package com.bridgeit;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -39,31 +37,27 @@ public class UserRegestrationPatterns implements IRegestration {
 
 		String pattern = "^[A-Z]{1}[a-z]{2,}";
 
-		try {
-			if (Pattern.matches(pattern, fName)) {
-				registration.setFirstName(fName);
-				return true;
-			} else
-				System.out.println("Invalid first name ");
-			return false;
-		} catch (NullPointerException ex) {
-			throw new InvalideUserDetailsException("enter valid user details");
+		if (fName == null || fName.isEmpty()) {
+			throw new InvalideUserDetailsException(InvalideUserDetailsException.ExeptionType.FIRST_NAME + " : enter valid user details");
 		}
+		if (Pattern.matches(pattern, fName)) {
+			registration.setFirstName(fName);
+			return true;
+		}
+		return false;
 	}
 
 	public boolean lastNamePattern() throws InvalideUserDetailsException {
 
 		String pattern = "^[A-Z]{1}[a-z]{2,}$";
-		try {
-			if (Pattern.matches(pattern, lName)) {
-				registration.setLastName(lName);
-				return true;
-			} else
-				System.out.println("invalid last name");
-			return false;
-		} catch (Exception e) {
-			throw new InvalideUserDetailsException("enter valid user details");
+		if (lName == null || lName.isEmpty()) {
+			throw new InvalideUserDetailsException(InvalideUserDetailsException.ExeptionType.LAST_NAME + "enter valid user details");
 		}
+		if (Pattern.matches(pattern, lName)) {
+			registration.setLastName(lName);
+			return true;
+		}
+		return false;
 	}
 
 	public void printResult() {
@@ -74,46 +68,44 @@ public class UserRegestrationPatterns implements IRegestration {
 	public boolean emailIdPattern() throws InvalideUserDetailsException {
 
 		String pattern = "^[a-zA-Z0-9]+([-_+.]?[a-zA-Z0-9])*[@]([A-Za-z0-9])+[.][A-Za-z]{2,}([.][A-Za-z]{2,}){0,1}$";
-		try {
-			if (Pattern.matches(pattern, email)) {
-				registration.setLastName(email);
-				System.out.println("valide mail");
-				return true;
-			} else
-				System.out.println("Invalid email as ");
-			return false;
-		} catch (Exception e) {
-			throw new InvalideUserDetailsException("enter valid user details");
+		if (email == null || email.isEmpty()) {
+			throw new InvalideUserDetailsException(InvalideUserDetailsException.ExeptionType.EMAIL_ID + " : enter valid user details");
 		}
+		if (Pattern.matches(pattern, email)) {
+			registration.setLastName(email);
+			System.out.println("valide mail");
+			return true;
+		} else
+			System.out.println("Invalid email as ");
+		return false;
 	}
 
 	public boolean phoneNumber() throws InvalideUserDetailsException {
 
-		try {
 			String pattern = "^[1-9]{2}[\\s][6-9][0-9]{9}$";
+			
+			if(phoneNumber == null || phoneNumber.isEmpty()) {
+				throw new InvalideUserDetailsException(InvalideUserDetailsException.ExeptionType.PHONE_NUMBER + " : senter valid user details");
+			}
 			if (Pattern.matches(pattern, phoneNumber)) {
 				registration.setPhoneNumber(phoneNumber);
 				return true;
 			} else
 				System.out.println("enter valid phone number ");
 			return false;
-		} catch (Exception e) {
-			throw new InvalideUserDetailsException("enter valid user details");
-		}
 	}
 
 	public boolean passwordPattern() throws InvalideUserDetailsException {
 
 		String pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,}$";
-		try {
+		if(password == null || password.isEmpty()) {
+			throw new InvalideUserDetailsException(InvalideUserDetailsException.ExeptionType.PASSWORD + " : enter valid user details");
+		}
 			if (Pattern.matches(pattern, password)) {
 				registration.setPassword(password);
 				return true;
 			} else
 				System.out.println("Invalide password ");
 			return false;
-		} catch (Exception e) {
-			throw new InvalideUserDetailsException("enter valid user details");
-		}
 	}
 }
